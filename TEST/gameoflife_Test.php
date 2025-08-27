c1141aa137f76fdfa72df07f1caf7975852a0b9d
minoru321 <minoru@example.com>
2025-08-27 16:30:01 +0400

    Added few more testings to the test file

--- TEST/gameoflife_Test.php
+++ TEST/gameoflife_Test.php
@@ -18,20 +18,68 @@
             echo ">> FAIL << \n";
         }
     echo "------------------------------------------------------------";
-    echo PHP_EOL;
+    echo PHP_EOL;usleep(500000);
 
 // Test 2 
-    echo "Test 2 : Checking Rule of the application (Neigbours = 4, so center should be die (*)\n";  
+    echo "Test 2 : Check Overpopulation (with 4 Neigbours  > center should  Die(*) \n\n";  
     $nextGenaration = applyRules($sampleGrid,3);
     printGrid($nextGenaration); // To check vissually print
     echo "------------------------------------------------------------";
-    echo PHP_EOL;
+    echo PHP_EOL;usleep(500000);
+
+//Test 3
+    echo "Test 3: Check Underpopulation - With 1 Neigbours > center should Die(*) \n\n";
+    $underpopGrid = [
+        [0 , 0, 0],
+        [0 , 1, 0],
+        [0 , 1, 0]
+    ];
+    $underpopNext = applyRules($underpopGrid,3);
+    printGrid($underpopNext);
+    echo "------------------------------------------------------------";
+    echo PHP_EOL;usleep(500000);
+
+// Test 4
+    echo "Test 4: Check Survival - With 2 Neigbours > center should Live(@) \n\n";
+    $surviveGrid = [
+        [0 , 0, 0],
+        [1 , 1, 0],
+        [0 , 1, 0]
+    ];
+    $surviveNext = applyRules($surviveGrid,3);
+    printGrid($surviveNext);
+    echo "------------------------------------------------------------";
+    echo PHP_EOL;usleep(500000);
+
+//Test 5
+    echo "Test 5: Check Survival - With 2 Neigbours > center should Live(@) \n\n";
+    $surviveGrid = [
+        [0 , 0, 0],
+        [1 , 1, 0],
+        [0 , 1, 0]
+    ];
+    $surviveNext = applyRules($surviveGrid,3);
+    printGrid($surviveNext);
+    echo "------------------------------------------------------------";
+    echo PHP_EOL;usleep(500000);
+
+// Test 6
+    echo "Test 5: Check Reproduction - With 3 Neigbours > center should Live(@) (Center was Dead cel) \n\n";
+    $reproductGrid = [
+        [0 , 0, 0],
+        [1 , 0, 1],
+        [0 , 1, 0]
+    ];
+    $reproductNext = applyRules($reproductGrid,3);
+    printGrid($reproductNext);
+    echo "------------------------------------------------------------";
+    echo PHP_EOL;usleep(500000);
 
 
-// Test 3
+// Test 7
     $grid = array_fill(0, 25, array_fill(0, 25, 0) );
     initializeGrid($grid);
-    echo "Test 3 : Checking Glider Placement in the start(Should be center)\n"; 
+    echo "Test 7 : Checking Glider Placement in the start(Should be center)\n\n"; 
     printGrid($grid);
     echo "------------------------------------------------------------";
     echo PHP_EOL;
