7830998683b83f51cbdf53a8f957a7ae637e67c7
minoru321 <minoru@example.com>
2025-08-27 14:17:44 +0400

    Updated initializeGrid function (Start should be in middle)

--- ALGO/gameoflife.php
+++ ALGO/gameoflife.php
@@ -5,13 +5,24 @@
     $grid = array_fill(0, $size, array_fill(0, $size, 0) ); # Filling $size * $size full matrix with 0 (All dead cells)
     
 
-    // Starting Glider pattern Display 
+    // Starting Glider pattern Display (Shoud be middle of the 25 * 25 metrix )
     function initializeGrid($grid){
-        $grid[1][2] = 1;
+        $centralX = 12;
+        $centralY = 12;
+    
+        $grid[$centralX - 1][$centralY] = 1;
+        $grid[$centralX][$centralY + 1] = 1;
+        $grid[$centralX + 1][$centralY - 1] = 1;
+        $grid[$centralX + 1] [$centralY - 1] = 1;
+        $grid[$centralX + 1][$centralY + 1] = 1;
+    
+    
+    
+    /*  $grid[1][2] = 1;
         $grid[1][2] = 1;
         $grid[1][3] = 1;
         $grid[2][3] = 1;
-        $grid[3][2] = 1;
+        $grid[3][2] = 1; */
     }
 
     // Print the full grid
@@ -19,9 +30,9 @@
         foreach ($grid as $row){
             foreach($row as $cell){
                 if($cell) {
-                    echo "0"; //live cell
+                    echo " 0"; //live cell
                 }else{
-                    echo "*"; // dead cell
+                    echo " *"; // dead cell
                 }
             }
             echo PHP_EOL;
@@ -30,7 +41,7 @@
 
     // Count the Live Neighbours
     function countLiveNeighbours($x,$y,$size,$grid){
-        $count = 0;  #for start all 0 live Neighbours(All dead)
+        $count = 0;  #for start all 0 live Neighbours(as all dead)
         for ($i = -1;$i <= 1 ;$i++) {
             for ($j = -1; $j <= 1 ; $j++){
                 if($i == 0 && $j == 0 ) continue; # we are caring about Neighbours (So skip center cell > [2][2]) 
@@ -46,7 +57,7 @@
 
     // Apply rules
     function applyRules($grid,$size) {
-        $newGrid = array_fill(0,$size,array_fill(0,$size,0)); # Filling new grid $size * $size full matrix with 0 (All dead cells)
+        $newGrid = array_fill(0,$size,array_fill(0,$size,0)); # Filling new grid $size * $size full metrix with 0 (All dead cells)
         for($x = 0; $x < $size ; $x++){
             for($y = 0; $y < $size ; $y++){
                 $neighbours = countLiveNeighbours($x,$y,$size,$grid);
@@ -68,8 +79,13 @@
     }
 }
 
+
+
+# initializeGrid($grid);
+
+
 // Run the functions
-for($tick = 0; $tick < 10; $tick++){
+for($tick = 1; $tick < 10; $tick++){
     echo "Genaration $tick\n";
     printGrid($grid);
     $grid = applyRules($grid,$size);
@@ -78,8 +94,7 @@ for($tick = 0; $tick < 10; $tick++){
 
 
 
-#initializeGrid($grid);
-#countLiveNeighbours(2,3);
+
 
 
 
